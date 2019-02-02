9d4b6067093324d6f212ab4376f6b93e729bf942
vahagn94 <vahagn94@gmail.com>
2019-02-02 11:01:13 -0800

    Activity UI

--- AndroidApp/Memeder/app/src/main/java/com/example/memeder/MainActivity.java
+++ AndroidApp/Memeder/app/src/main/java/com/example/memeder/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.memeder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button first_Btn;
+    private Button second_Btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
     }
 }

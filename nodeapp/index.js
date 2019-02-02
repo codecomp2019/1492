5d0de69c1f2b806036982584525e9dc79815aeb5
kmazas <kevin.mazas.528@my.csun.edu>
2019-02-02 10:37:06 -0800

    Added memes

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.get('/', (req, res) => res.send('Express Server for hosting memes for our app'));
+app.listen(3000, () => console.log('Hosting memes....'));
+
+app.use(express.static(__dirname + '/memes'));
